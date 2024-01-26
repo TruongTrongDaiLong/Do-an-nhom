3593b3e70300c435ab014d3120c04f633b07659b
Dai Trieu Phi <dtptrieuphidtp@gmail.com>
2024-01-26 10:47:08 +0700

    trieuphi tao form

---
+++ TrieuPhiForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_Git
+{
+    public partial class TrieuPhiForm : Form
+    {
+        public TrieuPhiForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TrieuPhiForm_Load(object sender, EventArgs e)
+        {
+            // Trieu Phi tao form 
+        }
+    }
+}
